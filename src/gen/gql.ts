14789641ccff7b0bb308fbe9f3db77b1a46898bf
star2000 <i@star2000.work>
2024-10-26 22:02:35 +0800

    清理冗余

--- src/gen/gql.ts
+++ src/gen/gql.ts
@@ -13,7 +13,7 @@ import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-  '\n        subscription heartbeat($uid: String!) {\n          heartbeat(uid: $uid)\n        }\n      ':
+  '\n      subscription heartbeat($uid: String!) {\n        heartbeat(uid: $uid)\n      }\n    ':
     types.HeartbeatDocument,
   '\n      subscription onlineCount {\n        onlineCount\n      }\n    ':
     types.OnlineCountDocument,
@@ -43,8 +43,8 @@ export function graphql(source: string): unknown
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(
-  source: '\n        subscription heartbeat($uid: String!) {\n          heartbeat(uid: $uid)\n        }\n      '
-): (typeof documents)['\n        subscription heartbeat($uid: String!) {\n          heartbeat(uid: $uid)\n        }\n      ']
+  source: '\n      subscription heartbeat($uid: String!) {\n        heartbeat(uid: $uid)\n      }\n    '
+): (typeof documents)['\n      subscription heartbeat($uid: String!) {\n        heartbeat(uid: $uid)\n      }\n    ']
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- src/utils/在线.ts
+++ src/utils/在线.ts
@@ -1,30 +1,19 @@
-import { useSubscription, UseSubscriptionReturn } from '@vue/apollo-composable'
+import { useSubscription } from '@vue/apollo-composable'
 import { uid } from 'quasar'
 import { graphql } from 'src/gen'
-import { HeartbeatSubscription } from 'src/gen/graphql'
 import { ref } from 'vue'
 
 const 我方编号 = uid()
-let 心跳:
-  | UseSubscriptionReturn<HeartbeatSubscription, { uid: string }>
-  | undefined = undefined
 const 在线人数 = ref()
 function 开始心跳() {
-  if (!心跳) {
-    心跳 = useSubscription(
-      graphql(`
-        subscription heartbeat($uid: String!) {
-          heartbeat(uid: $uid)
-        }
-      `),
-      { uid: 我方编号 }
-    )
-    心跳.onError(() => {
-      setTimeout(() => {
-        心跳?.restart()
-      }, 1000)
-    })
-  }
+  useSubscription(
+    graphql(`
+      subscription heartbeat($uid: String!) {
+        heartbeat(uid: $uid)
+      }
+    `),
+    { uid: 我方编号 }
+  )
 }
 function 订阅在线人数() {
   useSubscription(
